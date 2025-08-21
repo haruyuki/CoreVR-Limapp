8ae7c41b371418404b7f2298fda16f8b9089efa2
sq <isaac.j.robbins@gmail.com>
2025-08-21 17:03:08 +1000

    added ball test with target, began on sound

---
+++ Assets/Target.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject brokenParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BreakTarget()
+    {
+        Instantiate(brokenParticle, transform.position, Quaternion.identity);
+        transform.position = new Vector3(transform.position.x, transform.position.y + UnityEngine.Random.Range(-1f,1f), transform.position.z + UnityEngine.Random.Range(-1f,1f));
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collided");
+       if(other.gameObject == ball)
+        {
+
+            BreakTarget();
+        }
+    }
+}
