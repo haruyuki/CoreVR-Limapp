4ed01f04a2fc2101aa1f287b7bfc8f41d3af84b6
JaiBamforth <jai.bamforth@students.mq.edu.au>
2025-08-22 15:41:06 +1000

    created Jai test scene
    added a personal test scene to test some racket physics and some audio clips

---
+++ Assets/JaiRacket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaiRacket : MonoBehaviour
+{
+    void Update() 
+    {
+        //transform.Translate(1f, 0, 0);
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        
+        if (ball != null) 
+            {
+                ball.enabled = true;
+            }
+    }
+    
+}
