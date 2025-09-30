8d8fad428f52fb795b0581ed661ea4e8f3131f86
Oliver Lin <jumpy12359@gmail.com>
2025-09-30 13:04:40 +1000

    Make skybox not ugly

---
+++ Assets/CoreVR Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    public float rotationSpeed = 0.5f; // Adjust this value in the Inspector
+
+    void Update()
+    {
+        // Access the global RenderSettings and the current skybox material
+        // Set the "_Rotation" property based on time and rotationSpeed
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+    }
+}
